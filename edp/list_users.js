0804920a6c356b6ddf84f179067913d976b630c4
Alex Shekhovtsov <alex@choser.ai>
2026-06-12 13:17:36 +0000

    clean: remove table data, xlsx, archive from public repo

--- edp/list_users.js
+++
@@ -1,4 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('./data/choser.db');
-const users = db.prepare('SELECT id, username, role FROM users').all();
-console.log(JSON.stringify(users));
